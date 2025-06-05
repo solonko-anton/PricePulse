02f2264961db86db0c27b1b6102261a2abe17f53
solonko-anton <asolonko42@gmail.com>
2025-06-05 15:19:08 +0000

    added project settings

--- src/config/project_config.py
+++ src/config/project_config.py
@@ -4,11 +4,13 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     DB_ECHO: bool
     PROJECT_NAME: str
     VERSION: str
     DEBUG: bool
     CORS_ALLOWED_ORIGINS: str
+
 
 settings = Settings()
